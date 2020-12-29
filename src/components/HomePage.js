d08caf7679d0f5cbb033616a3979f0c11bc9d2ad
joshganote <j.ganote0825@gmail.com>
2020-12-29 13:12:27 -0600

    setting up routes and adding image

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const HomePage = () => {
+    return (
+        <div>
+            style={{
+          backgroundImage:
+            "url(" + require("assets/jtlgroupphoto.jpg") + ")",
+        }}
+        </div>
+    )
+}
+
+export default HomePage
