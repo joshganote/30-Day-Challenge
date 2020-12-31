d6e2461b6dbc636787b06bab8833aabb1c5ce3d0
joshganote <j.ganote0825@gmail.com>
2020-12-31 13:43:17 -0600

    created a back arrow to home page

--- src/components/MatthewBoda.js
+++ src/components/MatthewBoda.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { HiOutlineArrowUp } from 'react-icons/hi';
 
 import '../css/MatthewBoda.css';
 
 const MatthewBoda = () => {
+  const history = useHistory();
+  const HandleReturnToHome = () => {
+    history.push("/");
+  }
   return (
     <div className="page-background">
-      <h1 className="header">Matthew Boda</h1>
+      <div className="header-container">
+        <h1 className="header">Matthew Boda</h1>
+        <HiOutlineArrowUp
+        onClick={HandleReturnToHome}
+        color="#FFFFFF"
+        size="4em"
+        className="back-arrow"/>
+      </div>
     </div>
   )
 }
